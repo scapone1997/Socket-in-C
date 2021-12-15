a2d5dce15bb4f5d92c521c3454c43bf5ac84c444
scapone1997 <simo.capone.1997.7@outlook.it>
2021-12-15 11:32:04 +0100

    miglioramenti

---
+++ Client UDP/protocol.h
@@ -0,0 +1,18 @@
+/*
+ * protocol.h
+ *
+ *  Created on: 14 dic 2021
+ *      Author: simoc
+ */
+
+#ifndef PROTOCOL_H_
+#define PROTOCOL_H_
+
+#define ECHOMAX 255
+#define PORT 48000
+//default name server
+#define NAME "localhost"
+//default IP address
+#define IP "127.0.0.1"
+
+#endif /* PROTOCOL_H_ */
